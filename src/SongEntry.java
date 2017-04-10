edd50141ca3fc6b5a194e06e9c415ba6728ebaa3
jgoodson2 <jonathongoodson@clayton.edu>
2017-04-10 13:15:37 -0400

    Add private member fields to SongEntry

--- src/SongEntry.java
+++ src/SongEntry.java
@@ -2,4 +2,12 @@
  * Created by jgoodson2 on 4/10/17.
  */
 public class SongEntry {
+
+    private String uniqueID;
+    private String songName;
+    private String artistName;
+    private int songLength;
+    private SongEntry nextNode;
+
+
 }
